45ba7af44aaa620199f960d2319ac209e84026d9
Mike Gilbode <mike.gilbode@oracle.com>
2018-08-28 00:27:15 -0400

    Use Java10, create a healthcheck and put it on the main port and get the port from the environment variable PORT.

--- src/main/java/com/wercker/example/hellodropwizard/HelloDropwizardApplication.java
+++ src/main/java/com/wercker/example/hellodropwizard/HelloDropwizardApplication.java
@@ -1,8 +1,11 @@
 package com.wercker.example.hellodropwizard;
 
 import io.dropwizard.Application;
+import io.dropwizard.server.SimpleServerFactory;
 import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.jetty.HttpConnectorFactory;
 import io.dropwizard.setup.Environment;
+import com.wercker.example.hellodropwizard.health.HelloHealthCheck;
 import com.wercker.example.hellodropwizard.resources.AppVersionResource;
 
 public class HelloDropwizardApplication extends Application<HelloDropwizardConfiguration> {
@@ -23,7 +26,20 @@ public class HelloDropwizardApplication extends Application<HelloDropwizardConfi
     @Override
     public void run(final HelloDropwizardConfiguration configuration,
                     final Environment environment) {
+      SimpleServerFactory serverFactory = new SimpleServerFactory();
+      configuration.setServerFactory(serverFactory);
+
+      int port = 8080;
+      String portEnv = System.getenv("PORT");
+      if (portEnv != null) {
+        port = Integer.parseInt(portEnv);
+      }
+      HttpConnectorFactory connector = new HttpConnectorFactory();
+      connector.setPort(port);
+      serverFactory.setConnector(connector);
+
       environment.jersey().register(new AppVersionResource());
+      environment.healthChecks().register("hello", new HelloHealthCheck());
     }
 
 }
